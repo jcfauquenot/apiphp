b0ccc5466e7fc2647818fe293abc6da079a62341
Jean-Christophe Fauquenot <jcfauquenot@gmail.com>
2022-08-08 14:36:10 +0200

    Update Database.php
    delete info database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,8 +33,8 @@ class Database extends Config
     public $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => 'root',
-        'password' => 'lagrandemaison',
+        'username' => '',
+        'password' => '',
         'database' => 'ci4',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
